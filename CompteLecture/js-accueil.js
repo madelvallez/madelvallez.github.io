7c2e4ed03b297ef6177d838a7e1c58547bf1ddf6
madelvallez <70711790+madelvallez@users.noreply.github.com>
2021-07-13 23:55:28 +0200

    fix bug date

--- CompteLecture/js-accueil.js
+++ CompteLecture/js-accueil.js
@@ -6,7 +6,7 @@ function resu() {
 	var ake = parseInt(document.getElementById("stadeAke").value);
 	ake = isNaN(ake) ? 0 : ake;
 	var date = document.getElementById("jour").valueAsDate;
-	date = isNaN(date) ? new Date() : ake;
+	date = isNaN(date) ? new Date() : date;
 	
 	var date_fin = new Date("2021-08-29");
 	var diff = date_fin.getTime() - date.getTime();

15d7059b3dbd0c8e8078ee17a6686db17dc04878
madelvallez <70711790+madelvallez@users.noreply.github.com>
2021-07-14 00:19:37 +0200

    fix date issue

--- CompteLecture/js-accueil.js
+++ CompteLecture/js-accueil.js
@@ -6,7 +6,7 @@ function resu() {
 	var ake = parseInt(document.getElementById("stadeAke").value);
 	ake = isNaN(ake) ? 0 : ake;
 	var date = document.getElementById("jour").valueAsDate;
-	date = isTime(date) ? new Date() : date;
+	if ( date === null ) { date = new Date();}
 	
 	var date_fin = new Date("2021-08-29");
 	var diff = date_fin.getTime() - date.getTime();

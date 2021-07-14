e536712d8f2ad86d1af4ab54c07699d24b4b2203
madelvallez <70711790+madelvallez@users.noreply.github.com>
2021-07-14 09:45:59 +0200

    Set default date visible

--- CompteLecture/js-accueil.js
+++ CompteLecture/js-accueil.js
@@ -6,7 +6,11 @@ function resu() {
 	var ake = parseInt(document.getElementById("stadeAke").value);
 	ake = isNaN(ake) ? 0 : ake;
 	var date = document.getElementById("jour").valueAsDate;
-	if ( date === null ) { date = new Date();}
+	if ( date === null ) { 
+            date = new Date();
+            date.setHours(0, 0, 0, 0);
+            document.getElementById("jour").valueAsDate=date;
+        }
 	
 	var date_fin = new Date("2021-08-29");
 	var diff = date_fin.getTime() - date.getTime();

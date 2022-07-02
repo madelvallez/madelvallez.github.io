4e99494850d1dcd237305e564e09a0f83498b8f7
madelvallez <70711790+madelvallez@users.noreply.github.com>
2022-07-02 22:00:26 +0200

    renommage var js html neutre

--- CompteLecture/2022/js-accueil.js
+++ CompteLecture/2022/js-accueil.js
@@ -1,10 +1,21 @@
+//livre1 : id html
+//l1: nb de page lu entré par util
+
+
 function resu() {
-	var emile = parseInt(document.getElementById("stadeEmile").value);
-	emile = isNaN(emile) ? 0 : emile;
-	var contes = parseInt(document.getElementById("stadeAndersen").value);
-	contes = isNaN(contes) ? 0 : contes;
-	var ake = parseInt(document.getElementById("stadeAke").value);
-	ake = isNaN(ake) ? 0 : ake;
+	var l1 = parseInt(document.getElementById("Livre1").value);
+	l1 = isNaN(l1) ? 0 : l1;
+	var l2 = parseInt(document.getElementById("Livre2").value);
+	l2 = isNaN(l2) ? 0 : l2;
+	var l3 = parseInt(document.getElementById("Livre3").value);
+	l3 = isNaN(l3) ? 0 : l3;
+	var l4 = parseInt(document.getElementById("Livre4").value);
+	l4 = isNaN(l4) ? 0 : l4;
+	var l5 = parseInt(document.getElementById("Livre5").value);
+	l5 = isNaN(l5) ? 0 : l5;
+	var l6 = parseInt(document.getElementById("Livre6").value);
+	l6 = isNaN(l6) ? 0 : l6;
+
 	var date = document.getElementById("jour").valueAsDate;
 	if ( date === null ) { 
 		date = new Date();
@@ -13,7 +24,7 @@ function resu() {
 	}
 	var date_fin = document.getElementById("dernier").valueAsDate;
 	if ( date_fin === null ) { 
-		date_fin = new Date("2021-08-29");
+		date_fin = new Date("2022-08-29");
 		date_fin.setUTCHours(0, 0, 0, 0);
 		document.getElementById("dernier").valueAsDate=date_fin;
 	}
@@ -21,7 +32,7 @@ function resu() {
 	var diff = date_fin.getTime() - date.getTime();
 	var temps = diff / (1000 * 3600 * 24); 
 	
-	var pages = 1280 - (emile + contes + ake) ;
+	var pages = 1280 - (l1 + l2 + l3 + l4 + l5 + l6) ;
 	
 	var vitesse = pages / temps ;
 	
